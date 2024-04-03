8a5734f2faf0e3d9673e80f3188218d9e47de6bf
naveen <naveenjanaki2081@gmail.com>
2024-04-03 14:50:38 +0530

    ep-02

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
-
+import React from "react"
+import  ReactDOM  from "react-dom/client"
 const parent= React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"hello world"),React.createElement("h1",{},"hello world")]))
-
-
-
-
 const root= ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
