e8381b3dc7f5a9736bae42cfc6df9a3fe9d0d0eb
naveen <naveenjanaki2081@gmail.com>
2024-04-09 00:56:03 +0530

    ep-5

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Header from './components/Header';
+import Body from './components/Body';
+
+
+const currYear = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Copyright &copy; {currYear}, Made with 💗 by <strong>naveen</strong>
+      </p>
+    </footer>
+  );
+};
+
+const AppLayout = () => {
+  console.log(<Body />);
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
